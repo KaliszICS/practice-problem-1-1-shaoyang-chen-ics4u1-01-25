94a32647d244c3f5fca211c5ecf963c53931ec93
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-09-30 20:45:07 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -23,5 +23,28 @@ public class PracticeProblem {
 	public static void q5() {
 		//Write question 5 code here
 	}
+	public static int sum2D(int[][] arr2d) {
+        int sum = 0;
+        for (int i = 0; i < arr2d.length; i++) {
+            for (int j = 0; j < arr2d[i].length; j++) {
+                sum += arr2d[i][j];
+            }
+        }
+        return sum;
+    }
+    public static int sumRow(int[][] arr2d, int row) {
+        int sum = 0;
+        for (int i = 0; i < arr2d.length; i++) {
+            sum += arr2d[row][i];
+        }
+        return sum;
+    }
+    public static int sumColumn(int[][] arr2d, int col) {
+        int sum = 0;
+        for (int i = 0; i < arr2d.length; i++) {
+            sum += arr2d[i][col];
+        }
+        return sum;
+    }
 
 }
